8114fbac2f6b6dcfe86d07fdc4c30ae53f2232ae
J-unStiN <junstin119@gamil.com>
2023-12-20 20:22:36 +0900

    github action test

---
+++ src/main/java/com/example/githubActionsTest/GithubActionsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.githubActionsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsTestApplication.class, args);
+		System.out.println("Hello Spring!!!");
+	}
+
+}
